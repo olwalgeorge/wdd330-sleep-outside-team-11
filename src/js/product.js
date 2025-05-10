d49b188f4c66bd74dd258448355a974b45008504
hlaolil <hla24002@byui.edu>
2025-05-10 22:18:01 +0200

    latest solution to the add item

--- src/js/product.js
+++ src/js/product.js
@@ -4,11 +4,19 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  let cart = getLocalStorage("so-cart") || [];
+  let cart = getLocalStorage("so-cart");
+  // Check if cart exists and is an array
+  if (!cart) {
+    cart = [];
+  } else if (!Array.isArray(cart)) {
+    // convert cart into an array if cart exists but is not an array
+    cart = [cart];
+  }
+  // Add the new product to the cart array
   cart.push(product);
+  // Save the updated cart back to localStorage
   setLocalStorage("so-cart", cart);
 }
-
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);

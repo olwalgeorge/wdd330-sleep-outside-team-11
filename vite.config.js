f8b89af8c4496d036b0a6597770426b5a23eca97
George Olwal <olwalgeorge@live.com>
2025-05-13 21:09:26 +0300

    removed single hardcorded html and their rffferences

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   root: "src/",
   publicDir: "../public",
-
   build: {
     outDir: "../dist",
     rollupOptions: {
@@ -12,21 +11,9 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
-        product1: resolve(
-          __dirname,
-          "src/product_pages/cedar-ridge-rimrock-2.html",
-        ),
-        product2: resolve(__dirname, "src/product_pages/marmot-ajax-3.html"),
-        product3: resolve(
-          __dirname,
-          "src/product_pages/northface-alpine-3.html",
-        ),
-        product4: resolve(
-          __dirname,
-          "src/product_pages/northface-talus-4.html",
-        ),
+        productDetail: resolve(__dirname, "src/product_pages/index.html"),
       },
-    },    // Ensure static assets are copied to dist
+    },// Ensure static assets are copied to dist
     assetsInclude: ["**/*.json", "**/*.svg"],
     copyPublicDir: true,
   },

497d2b8b8342368609cb119608a512b4e7611009
hlaolil <hla24002@byui.edu>
2025-05-18 20:58:30 +0200

    Pulled Changes

--- src/js/cart.js
+++ src/js/cart.js
@@ -6,8 +6,8 @@ function renderCartContents() {
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
     document.querySelector(".product-list").innerHTML = htmlItems.join("");
   } else {
-    document.querySelector(".product-list").innerHTML = 
-      "<li class=\"cart-empty\">Your cart is empty</li>";
+    document.querySelector(".product-list").innerHTML =
+      '<li class="cart-empty">Your cart is empty</li>';
   }
 }
 

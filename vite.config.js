9636477ca55fb31899270499529c687fff361f47
John Bright Agbons <brightinojohnny@gmail.com>
2025-05-22 05:54:36 +0100

    fixed a break in build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   root: "src/",
   publicDir: "../public",
-
   build: {
     outDir: "../dist",
     rollupOptions: {
@@ -12,8 +11,12 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
+        productDetail: resolve(__dirname, "src/product_pages/index.html"),
         product: resolve(__dirname, "src/product/index.html"),
       },
     },
+    // Ensure static assets are copied to dist
+    assetsInclude: ["**/*.json", "**/*.svg"],
+    copyPublicDir: true,
   },
 });
